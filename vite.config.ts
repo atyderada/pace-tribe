7e486e581760b685fa1699795a0d136fa5172973
Anthony De Rada <anthony@megabadvisors.com>
2025-01-10 18:03:44 -0400

    Initial Commit

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/pace-tribe/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
